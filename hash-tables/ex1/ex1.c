62b4acdd82a845a5cdab7a0bb2ab8f576902f507
tsaihuangsd <tsaihuangsd@gmail.com>
2019-01-18 11:11:55 -0800

    end of challenge submission

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -5,10 +5,36 @@
 
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
+  //build the hash table with each key equals the difference between limit and current weight in the weights array, and each value equals the weights index of the current weight
   HashTable *ht = create_hash_table(16);
-
-  // YOUR CODE HERE
-
+  int difference;
+  for (int i=0; i<length; i++){
+    difference = limit - weights[i];
+    // for (int j=i+1; j<length; j++){
+    //   if (weights[j] == difference){
+        hash_table_insert(ht, difference, i);
+      // }
+    // }
+  }
+  //loop through the weights array.  For each weight, find the difference between limit and weight, then use the difference as the key to retrieve a value in the hash table.  
+  for (int j=0; j<length; j++){
+    difference = limit - weights[j];
+    int weights_index = hash_table_retrieve(ht, difference);
+    //If the retrieved value exists (i.e. != -1), compare the current weight to the difference, assign new_answer->index_1 to the bigger one and new_answer->index_2 to the smaller, then return new_answer.  If retrieved value does not exist (i.e. == -1), then return NULL.
+    if ((weights_index != -1) && ((weights_index < length) && (weights_index >= 0))){
+      Answer *new_answer = malloc(sizeof(Answer*));
+      printf("current weight: %d, difference: %d\n", weights[j], difference);
+      if (weights[j] > difference ){
+        new_answer->index_1 = hash_table_retrieve(ht, weights[j]);
+        new_answer->index_2 = hash_table_retrieve(ht, difference);
+      }
+      else{
+        new_answer->index_1 = hash_table_retrieve(ht, difference);
+        new_answer->index_2 = hash_table_retrieve(ht, weights[j]);
+      }
+      return new_answer;
+    }
+  }
   return NULL;
 }
 
